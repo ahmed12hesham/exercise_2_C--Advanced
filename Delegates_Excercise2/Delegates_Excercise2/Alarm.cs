35a5190b65eb3816af1d1d0e32c8659aea1dbf83
Ahmed hesham mansour <38839663+ahmed12hesham@users.noreply.github.com>
2025-06-18 18:15:00 +0300

    Add files via upload

---
+++ Delegates_Excercise2/Delegates_Excercise2/Alarm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Excercise2
+{
+    internal class Alarm
+    {
+        public int AlarmValue { get; set; }  // fresch hold 
+
+        public Alarm(int alarmTemp) 
+        {
+
+            AlarmValue = alarmTemp;
+        }
+        public void SetAlarmValue(int newAlarmValue) 
+        {
+            AlarmValue = newAlarmValue;
+        }
+        public void FireAlarm(object? objecct, TempArgs tempArgs)
+        {
+            if (tempArgs.Temprature > AlarmValue)
+            {
+                Console.WriteLine($"TAKE CARE, tHE TEMP VALUE {tempArgs.Temprature} is greater than {AlarmValue}");
+            }
+        }
+    }
+}
